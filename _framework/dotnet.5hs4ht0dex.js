d843994396f5daa4355f705f810c468b0b4701a2
Local Azure DevOps Emulator <local-azure-devops@localhost>
2026-09-16 20:49:26 +0200

    deploy: LogisticsWebApp 2026-09-16 20:49:26

--- _framework/dotnet.5hs4ht0dex.js
+++ _framework/dotnet.5hs4ht0dex.js
@@ -524,10 +524,10 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "debugLevel": 0,
   "linkerEnabled": true,
   "appsettings": [
-    "../appsettings.Production.json",
-    "../appsettings.json",
+    "../appsettings.Development.json",
     "../appsettings.github.json",
-    "../appsettings.Development.json"
+    "../appsettings.json",
+    "../appsettings.Production.json"
   ],
   "globalizationMode": "sharded",
   "extensions": {
